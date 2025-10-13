12d6b7b35385da8402bbccbb240c8bb0442ff4db
Duonye <148512627+Duonye@users.noreply.github.com>
2025-10-12 21:07:53 -0400

    Created RegisterRequest.cs
    Password confirmation and strength rules

---
+++ ITAssetManager.Web/Models/ViewModels/RegisterRequest.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ITAssetManager.Web.Models.ViewModels
+{
+    public class RegisterRequest
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
